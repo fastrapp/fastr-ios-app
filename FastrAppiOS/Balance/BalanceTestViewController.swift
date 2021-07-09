dd237dedef7b94eaca43ff1bcf33f43873f8af38
Jonathan <jonathan.tiao17@gmail.com>
2021-07-09 14:21:38 -0400

    updated no button to put through couple tests

--- FastrAppiOS/Balance/BalanceTestViewController.swift
+++ FastrAppiOS/Balance/BalanceTestViewController.swift
@@ -16,6 +16,7 @@ protocol BalanceTestViewControllerDelegate:class{
 class BalanceTestViewController : UIViewController {
     
     var pageTitle = "Have you noticed any changes in your balance?"
+    var numNos = 0
     @IBOutlet weak var pageLabel: UILabel!
     weak var delegate:BalanceTestViewControllerDelegate?
     override func viewDidLoad() {
@@ -25,7 +26,22 @@ class BalanceTestViewController : UIViewController {
         delegate?.didCompleteBalanceTest(hasBalanceChange: true)
     }
     @IBAction func onNoButtonTap(sender: UIButton){
+        if numNos == 0 {
+            pageLabel.text = "If you feel comfortable doing so, please stand up. Did you notice any new difficulty in standing up?"
+            numNos += 1
+        }
+        else if numNos == 1{
+            pageLabel.text = "Please walk forward as far as you are comfortable. Does this feel any different than normal for you?"
+            numNos += 1
+        }
+        else if numNos == 2{
+            pageLabel.text = "Please close your eyes while standing. Do you feel like you are going to fall over?"
+            numNos += 1
+        }
+        else if (numNos == 3)
+        {
         delegate?.didCompleteBalanceTest(hasBalanceChange: false)
+        }
     }
     
 }
