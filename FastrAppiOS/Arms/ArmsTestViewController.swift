8aaf9f4f607659d3270eee2d40aa8c6dfc623728
Jonathan <jonathan.tiao17@gmail.com>
2021-07-12 16:24:08 -0400

    initial button work

--- FastrAppiOS/Arms/ArmsTestViewController.swift
+++ FastrAppiOS/Arms/ArmsTestViewController.swift
@@ -12,9 +12,37 @@ import UIKit
 class ArmsTestViewController : UIViewController {
     
     @IBOutlet weak var pageLabel: UILabel!
+    @IBOutlet weak var startButton : UIButton!
+    @IBOutlet weak var testButton : UIButton!
+    
+    //keep track of whether tapping is being done with left versus right hand
+    var rightHand = true
+    
+    //keep track of how many times user has tapped the button
+    var tapsCounter = 0
+    
+    //keep track of how long in between taps
+    var tapsInterval = [Double]()
+    
+    //timer with last tap to get intervals
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        startButton.layer.cornerRadius = 4
+        testButton.layer.cornerRadius = 4
+        testButton.isHidden = true
+        for _ in 0...10{
+            tapsInterval.append(-1.0)
+        }
     }
     
+    @IBAction func onStartButtonTap(sender: UIButton){
+        startButton.isHidden = true
+        testButton.isHidden = false
+    }
+    @IBAction func onTestButtonTap (sender: UIButton){
+        if tapsCounter < 10 {
+            
+        }
+    }
 }
